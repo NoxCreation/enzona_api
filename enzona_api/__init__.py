9828f423cd96f4cbbac590d0256f1e1de0b53302
dennisbr91 <beltrandennis06@gmail.com>
2022-07-30 20:30:54 -0400

    Change import Product to mProduct in init.py

--- enzona_api/__init__.py
+++ enzona_api/__init__.py
@@ -14,7 +14,7 @@ __author__ = 'Josué Carballo Baños'
 __license__ = 'GNU v3'
 __ver_major__ = 0
 __ver_minor__ = 0
-__ver_patch__ = 3
+__ver_patch__ = 9
 __ver_sub__ = ''
 __version__ = "%d.%d.%d%s" % (__ver_major__, __ver_minor__,
                               __ver_patch__, __ver_sub__)
